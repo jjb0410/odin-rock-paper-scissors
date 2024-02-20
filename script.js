1691f6b111aca740792a6c25f655b6e9b2eb75bf
jjb0410 <154564136+jjb0410@users.noreply.github.com>
2024-02-20 18:03:22 -0500

    Add comments to explain code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let computerSelection = getComputerChoice();
-let playerSelection = prompt('Rock, paper, or scissors?', 'Rock');
+let computerSelection = getComputerChoice(); //initialize choice for computer 
+let playerSelection = prompt('Rock, paper, or scissors?', 'Rock'); // take player choice
 
 function getComputerChoice() {
-    let choice = (Math.random() * 3);
+    let choice = (Math.random() * 3); //computer choice is 1/3 chance of rock paper or scissors
     if (choice >= 0 && choice < 1) {
         return "Rock";
     }
@@ -15,39 +15,45 @@ function getComputerChoice() {
 }
 
 function randomizeChoice() {
-    computerSelection = getComputerChoice();
+    computerSelection = getComputerChoice(); //rolls a new random choice for computer
 }
 
 function playRound(playerSelection, computerSelection) {
-    randomizeChoice();
-    computerSelection = computerSelection.toLowerCase();
+    randomizeChoice(); //gets a new random choice for the computer for each new round
+    computerSelection = computerSelection.toLowerCase(); //convert to lowercase to avoid case sensitivity issues
     playerSelection = playerSelection.toLowerCase();
 
+    //check for winning choices
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
     (playerSelection == 'paper' && computerSelection == 'rock') ||
     (playerSelection == 'scissors' && computerSelection == 'paper')) {
             return "You win!";
         }
+    //check for losing choices
     else if ((playerSelection == 'rock' && computerSelection == 'paper') ||
     (playerSelection == 'paper' && computerSelection == 'scissors') ||
     (playerSelection == 'scissors' && computerSelection == 'rock')) {
         return "You lose!";
     }
+    //remaining choices should be a tie
     else {
         return "It's a tie!";
     }
 }
 
-
+//plays 5 games in total
 function playGame() {
-    let playerScore = 0;
+    let playerScore = 0; //initialize scores at 0
     let computerScore = 0;
     
+    //i don't know loops yet but i already feel bad copy and pasting this 5 times
     playRound(playerSelection, computerSelection);
 
+    // if player wins add 1 to score
     if (playRound(playerSelection, computerSelection) == "You win!") {
         playerScore += 1;
     }
+    // if computer wins add 1 to score
     else if (playRound(playerSelection, computerSelection)== "You lose!") {
         computerScore += 1;
     }
@@ -88,14 +94,15 @@ function playGame() {
         computerScore += 1;
     }
 
+    //check if player score is higher than computer score, if so, declare player winner
     if (playerScore > computerScore) {
         return "You win! Score: " + playerScore + " to " + computerScore;
     }
-
+    //check if computer score is higher than player score, if so, declare player loser
     else if (computerScore > playerScore) {
         return "You lose! Score: " + playerScore + " to " + computerScore;
     }
-
+    //only other option is a tie
     else {
         return "You tied! Score: " + playerScore + " to " + computerScore;
     }
